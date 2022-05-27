abe664a6a9d5e2d4d447d3ad98d0f58afe049b52
reds362 <cbweng0924@yahoo.comtw>
2022-05-27 17:05:48 +0800

    check file exist

--- products.py
+++ products.py
@@ -1,14 +1,19 @@
+import os
 products = []
-
-
 #讀取檔案
-with open('product.csv','r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name,price = line.strip().split(',')
-		products.append([name,price])
-print(products)
+if os.path.isfile('product.csv'):#檢查檔案是否在不在
+	print('找到了檔案!!')
+
+	with open('product.csv','r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name,price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案!!')
+
 	
 
 #使用者輸入
