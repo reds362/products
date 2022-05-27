51a7c1143a7a222d2345f63749069adfdd4beeca
reds362 <cbweng0924@yahoo.comtw>
2022-05-27 16:45:30 +0800

    read write file

--- products.py
+++ products.py
@@ -1,52 +1,36 @@
 products = []
 
 
-
+#讀取檔案
 with open('product.csv','r', encoding = 'utf-8') as f:
-	
 	for line in f:
-		#s = line.strip().split(',')
-		#print(s)
-		#name.append(s[0])
-		#price.append(s[1])
+		if '商品,價格' in line:
+			continue
 		name,price = line.strip().split(',')
 		products.append([name,price])
-
 print(products)
 	
 
-
-
-
-
+#使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
 	products.append([name,price])
-	#p = [name,price]
-	#products.append(p)
 print(products)
 
-#print(products[0][0])	
-#print(products[0][1])
-#print(products[1][0])
-#print(products[1][1])
 
+#購買清單
 for p in products:
 	print(p[0] , '的價格為' , p[1])
 
 
-#with open('product.txt','w') as f:
-#	for p in products:
-#		#print(p)
-#		#f.write(p) #type Error not list 
-#		f.write(p[0] +"," +p[1]+'\n')
-
+#寫入檔案
 with open('product.csv','w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
-		#print(p)
-		#f.write(p) #type Error not list 
 		f.write(p[0] +"," +p[1]+'\n')
+
+
+
